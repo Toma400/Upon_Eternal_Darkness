86e3f87bbe5123f57b0feef223783f28bb656ef7
Toma400 <thomasso_calgori@o2.pl>
2022-06-25 10:14:35 +0200

    Added variables

--- src/main/java/toma400/ued/UedMain.java
+++ src/main/java/toma400/ued/UedMain.java
@@ -2,6 +2,9 @@ package toma400.ued;
 
 public class UedMain {
 
+    public static final String VERSION_PHASE = "alpha";
+    public static final Integer VERSION_NUMBER = 1;
+
     public static void main (String args[]){
 
     }
